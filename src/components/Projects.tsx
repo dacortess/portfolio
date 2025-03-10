19653de3881b3131a981a79e0144769a1dff030b
David <70292031+dacortess@users.noreply.github.com>
2025-03-10 12:46:12 -0500

    update: crypto project link
    Signed-off-by: David <70292031+dacortess@users.noreply.github.com>

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -14,7 +14,7 @@ const Projects = () => {
       description: t('projects.projects.cryptoAnalyzer'),
       tech: ['Python', 'Flask', 'Typescript', 'Vite', 'TailwindCSS'],
       image: cryptoanalyzerimg,
-      demo: 'https://playful-flan-1c5e1c.netlify.app/',
+      demo: 'https://cryptohubunal.netlify.app/',
       github: 'https://github.com/dacortess/Intro-Crypto-Project?tab=readme-ov-file',
     },
     {
